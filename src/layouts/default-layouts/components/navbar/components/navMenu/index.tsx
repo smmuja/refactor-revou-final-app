d55be2525ca92af88dfdcb1735742cef25a93414
smmuja <smmuja27@gmail.com>
2024-05-13 22:22:51 +0700

    minor fix

--- src/layouts/default-layouts/components/navbar/components/navMenu/index.tsx
+++ src/layouts/default-layouts/components/navbar/components/navMenu/index.tsx
@@ -3,23 +3,21 @@ import { Link } from "react-router-dom";
 
 export function NavMenu() {
   return (
-    <>
-      <div className="flex  gap-3">
-        <Link
-          to="/business"
-          className="flex gap-3 p-3  rounded-sm hover:bg-slate-300 max-h-fit"
-        >
-          <BusinessIcon />
-          <span>Business</span>
-        </Link>
-        <Link
-          to="/product"
-          className="flex gap-3 p-3 rounded-sm hover:bg-slate-300"
-        >
-          <ProductIcon />
-          <span>Product</span>
-        </Link>
-      </div>
-    </>
+    <div className="flex gap-3">
+      <Link
+        to="/business"
+        className="flex gap-3 p-3 rounded-sm hover:bg-slate-300 max-h-fit"
+      >
+        <BusinessIcon />
+        <span>Business</span>
+      </Link>
+      <Link
+        to="/product"
+        className="flex gap-3 p-3 rounded-sm hover:bg-slate-300"
+      >
+        <ProductIcon />
+        <span>Product</span>
+      </Link>
+    </div>
   );
 }
