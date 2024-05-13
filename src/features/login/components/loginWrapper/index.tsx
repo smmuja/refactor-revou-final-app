1454216cfca9b9d9bbbab8000e32f1cdc738b50f
yuliussmayoru <yuliussmayoru@gmail.com>
2024-05-13 21:47:30 +0700

    update

--- src/features/login/components/loginWrapper/index.tsx
+++ src/features/login/components/loginWrapper/index.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "features/login";
 export function LoginWrapper() {
     return (
         <div className="h-[100svh] flex justify-center items-center">
-        <LoginForm/>
-    </div>
+            <LoginForm/>
+        </div>
     )
 }
