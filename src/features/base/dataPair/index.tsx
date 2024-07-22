cd9c4917ef198c70522398bbd10d262859a5bd7e
smmuja <smmuja27@gmail.com>
2024-07-22 17:22:07 +0700

    component base: add not found component

--- src/features/base/dataPair/index.tsx
+++ src/features/base/dataPair/index.tsx
@@ -5,8 +5,8 @@ export function DataPair(props: DataPairProps) {
   const { label, data, className } = props;
 
   return (
-    <div className={twMerge("flex w-full gap-1", className)}>
-      <span className="w-2/5 ">{label}</span>
+    <div className={twMerge("flex w-full gap-1 my-3", className)}>
+      <span className="w-2/5  font-medium">{label}</span>
       <span className="w-3/5 ">{data}</span>
     </div>
   );
