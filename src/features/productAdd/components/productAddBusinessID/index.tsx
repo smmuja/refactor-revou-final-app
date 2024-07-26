fb4ccb12cdc70d84c4519fc4f3687ce2f2c0aafd
smmuja <smmuja27@gmail.com>
2024-07-27 00:54:42 +0700

    function: product add

--- src/features/productAdd/components/productAddBusinessID/index.tsx
+++ src/features/productAdd/components/productAddBusinessID/index.tsx
@@ -1,12 +1,23 @@
+import { useGetUser } from "features/profile/hooks";
+
 export function ProductAddBusinessID() {
+  const { UserBusiness } = useGetUser();
+
   return (
-    <div className="mb-5">
-      <label htmlFor="">Business ID</label>
-      <br />
-      <select name="businessId" id="" className="w-full py-3 rounded-md">
-        <option value="">Business 1</option>
-        <option value="">Business 2</option>
-      </select>
-    </div>
+    <>
+      <div className="mb-5">
+        <label htmlFor="">Business ID</label>
+        <br />
+        <select name="business_id" id="" className="w-full py-3 rounded-md">
+          {UserBusiness?.map((item) => {
+            return (
+              <option key={item.id} value={item.id}>
+                {item.business_name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </>
   );
 }
