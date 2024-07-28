59dd6c4cc6cebee28f05beb5dee9f2c3a11e9da0
smmuja <smmuja27@gmail.com>
2024-07-28 10:37:53 +0700

    feat: product detail function

--- src/features/productsDetail/components/productDetailEditButton/index.tsx
+++ src/features/productsDetail/components/productDetailEditButton/index.tsx
@@ -1,10 +1,15 @@
-import { productsEditUrl } from "config";
+import { productsUrl } from "config";
 import { Button } from "features/base";
+import { useGetProductDetail } from "features/productsDetail/hooks";
 import { useNavigate } from "react-router-dom";
 
 export function ProductDetailEditButton() {
+  const { data } = useGetProductDetail();
+
   const navigate = useNavigate();
-  const handleProductEdit = () => navigate(productsEditUrl);
+  const handleProductEdit = () => navigate(`${productsUrl}/${data.id}/edit`);
+
+  console.log(data.id);
   return (
     <Button
       onClick={handleProductEdit}
